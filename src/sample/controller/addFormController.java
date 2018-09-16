9e26242beee611fb09fa0d6f270ff1b2956985a8
Kevin Ho <kevinho479@gmail.com>
2018-09-15 23:28:22 -0400

    added the ui for adding a job listing

--- src/sample/controller/addFormController.java
+++ src/sample/controller/addFormController.java
@@ -1,4 +1,36 @@
 package sample.controller;
 
-public class addFormController {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddFormController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXTextField companyTextField;
+
+    @FXML
+    private JFXTextField linkTextField;
+
+    @FXML
+    private JFXTextField titleTextField;
+
+    @FXML
+    private JFXTextField locationTextField;
+
+    @FXML
+    private JFXButton saveButton;
+
+    @FXML
+    void initialize() {
+
+    }
 }
